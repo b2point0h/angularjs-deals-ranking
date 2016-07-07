daa8e21b970df84ff9637f34b953fc446474dc1c
Brandon Leung <info@re-brand.us>
2016-07-07 09:36:22 -0700

    Adding new types of custom filters, trying to pass in min and max arguments.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,27 @@ app.controller('GameController', function($scope, $http) {
     $http.get("http://www.cheapshark.com/api/1.0/deals")
     .then(function(response) {
         $scope.deals = response.data;
+
+        $scope.byRange90 = function(val) {
+          return (val.metacriticScore >= 90)
+        }
+        $scope.byRange80 = function(val) {
+          return (val.metacriticScore >= 80 && val.metacriticScore <=90)
+        }
+        $scope.rangeFilter = function (val, min, max) {
+            return (val.metacriticScore > games.min && val.metacriticScore < games.max);
+        }
+        app.filter('range', function(){
+          return function(val, min, max){
+            var output = [];
+            output = val.metacriticScore >= min && val.metacriticScore <= max;
+            return output;
+          }
+        });
+        $scope.range = function (val, min, max) {
+          var score = Math.ceil(val.metacriticScore);
+            return (val.metacriticScore > score.min && val.metacriticScore < score.max);
+        };
+        
     });
 });
